52d25053f45be4fd8b7392f4eec37783a4681fe7
Ihor <igornepipenko@gmail.com>
2020-01-26 13:20:00 +0200

    Feature: mixin  [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,54 @@
-class Singleton {
-    private static _instance: Singleton;
+type Constructable = new (...args: any[]) => {};
 
-    private constructor() {
-    }
-
-    public static getInstance(): Singleton {
-        if (!Singleton._instance) {
-            Singleton._instance = new Singleton();
-        }
-        return Singleton._instance;
+function Timestamp<BaseClass extends Constructable>(BC: BaseClass) {
+    return class extends BC {
+        public timestamp = new Date();
     }
 }
 
-const inst1 = Singleton.getInstance();
-const inst2 = Singleton.getInstance();
-const inst3 = Singleton.getInstance();
+function Tagged<BaseClass extends Constructable>(BC: BaseClass) {
+    return class extends BC {
+        public tags = ['ts', 'js'];
+    }
+}
+
+class BasePoint {
+    public x: number;
+    public y: number;
+    private _z: number = 1;
+
+    public constructor({x, y}: { x: number, y: number }) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public getSum(): number {
+        return this.x + this.y
+    }
+
+    public get z(): number {
+        return this._z;
+    }
+
+    public set z(val) {
+        this._z = val;
+    }
+}
+
+
+let p1 = new BasePoint({x: 1, y: 1});
+
+
+class Point extends Timestamp(Tagged(BasePoint)) {
+    public constructor(coords: { x: number, y: number }) {
+        super(coords);
+    }
+
+    public getSum(): number {
+        return super.getSum();
+    }
+}
+
+let p2 = new Point({x: 1, y: 1});
+
+

bf2ebeb0375490204021d805f7131d4eb390d9fb
Ihor <igornepipenko@gmail.com>
2020-01-26 11:26:13 +0200

    Feature: function base [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,58 @@
-import { generateMenu } from "./menu/menu";
-import { list } from "./menu/data";
-$('.menu')
-    .html(generateMenu(list))
-    .on('click', (e: Event)=>{
-        const el: HTMLElement = e.target as HTMLElement;
-        if (!el.classList.contains('title')) {
-            return;
-        }
-        const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
-        parentLi.classList.toggle('menu-open');
-    })
-// const navMenu: HTMLDivElement = document.querySelector('.menu') as HTMLDivElement;
-// navMenu.innerHTML = generateMenu(list);
-// navMenu.addEventListener('click', (e: MouseEvent) => {
-//     const el: HTMLElement = e.target as HTMLElement;
-//     if (!el.classList.contains('title')) {
-//         return;
+// function average(a: number, b: number, c: number): string {
+//     const avg: number = (a + b + c) / 3;
+//     return `Average is ${avg}`;
+// }
+//
+// average(1, 2);
+// average(1, 2, 's');
+// const num: number = average(1, 2, 1);
+
+// function average(a: number, b?: number, c?: number): string {
+//     if (b === undefined) {
+//         b = 0;
 //     }
-//     const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
-//     parentLi.classList.toggle('menu-open');
-// })
+//     if (c === undefined) {
+//         b = 0;
+//     }
+//     const avg: number = (a + b + c) / 3;
+//     return `Average is ${avg}`;
+// }
+//
+// average(1);
+// average(1, 2);
+// average(1, 2, 's');
+// const num: number = average(1, 2, 1);
+
+// function average(a: number, b: number = 0, c: number = 0): string {
+//     const avg: number = (a + b + c) / 3;
+//     return `Average is ${avg}`;
+// }
+//
+// average(1);
+// average(1, 2);
+// average(1, 2, 's');
+// const num: number = average(1, 2, 1);
+type sn = string | number;
+
+function isString(a: sn): a is string {
+    return typeof a === 'string'
+}
+
+function average(...args: sn[]): string {
+    let total: number = 0;
+    for (const a of args) {
+        if (isString(a)) {
+            total += parseInt(a, 10);
+            continue;
+        }
+        total += a;
+    }
+    const avg = total/args.length;
+    return `Average is ${avg}`;
+}
+
+average(1);
+average(1, 2);
+average(1, 2, 's');
+average(1, 2, 's', 'asdasd', '1231', '123123', 123123);
+const num: number = average(1, 2, 1);
